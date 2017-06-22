00ff272519ae59f74d0315e263c85215f191ed29
严俊东 <1172482914@qq.com>
2017-06-22 18:44:57 +0800

    css map

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var reload = browserSync.reload;
 var config = require('./config.json');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
 var port = config.port;
 
 // 静态服务器 + 监听 scss/html 文件
@@ -22,7 +23,9 @@ gulp.task('serve', ['sass'], function () {
 gulp.task('sass', function () {
   return gulp.src(["stylesheet/scss/**/*.scss", "!stylesheet/scss/**/_*.scss"])
   .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+  .pipe(sourcemaps.init())
   .pipe(sass())
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest("stylesheet/css"))
   .pipe(reload({stream: true}));
 });

e95277491bf710fa5abee93f3c6fde4d85db86b4
严俊东 <1172482914@qq.com>
2017-06-23 16:40:41 +0800

    头部优化

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
-var menuDown = false;
-$('#menu-link').click(function (e) {
-  e.stopPropagation();
-  if (!menuDown){
-    $('#menu').slideDown(400);
-    menuDown = true;
+(function () {
+  var menu = $('#menu') // 菜单
+  var menuLink = $('#menu-link') // 隐藏菜单的触发按钮
+  var appShade = $('#app-shade') // 内容幕布
+  var app = $('#app') // 整个视口内容
+  menu.attr('data-show', '0') // 默认标记位隐藏 (0 为隐藏，1 为 显示)
+  /**
+   * 滑出菜单
+   * */
+  function MenuIn() {
+    menu.animate({
+      'right': '20%'
+    }, 400);
+    app.animate(({
+      'left': '80%'
+    }))
+    appShade.addClass('active')
+    menu.attr('data-show', '1')
   }
-  else {
-    $('#menu').slideUp(400);
-    menuDown = false;
+  /**
+   * 隐藏菜单
+   * */
+  function MenuOut() {
+    menu.animate({
+      'right': '100%'
+    }, 400);
+    app.animate(({
+      'left': '0'
+    }))
+    appShade.removeClass('active')
+    menu.attr('data-show', '0')
   }
-});
-$(document).click(function (e) {
-  menuDown = false;
-  $('#menu').fadeOut(400);
-});
+  /*监听菜单的触发按钮的点击*/
+  menuLink.click(function (e) {
+    e.stopPropagation();
+    if(Number(menu.attr('data-show'))) {
+      MenuOut()
+    }else {
+      MenuIn()
+    }
+  })
+  /*监听页面点击事件*/
+  $(document).click(function (e) {
+    MenuOut()
+  });
+}())

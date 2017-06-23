c6e4ec1bfb4ff437513b470230d1ba4b336e8ac4
严俊东 <1172482914@qq.com>
2017-06-23 16:46:35 +0800

    头部fixed兼容性问题

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
   var menu = $('#menu') // 菜单
   var menuLink = $('#menu-link') // 隐藏菜单的触发按钮
   var appShade = $('#app-shade') // 内容幕布
+  var headFixed = $('#head-fixed') // 头部置顶的部分
   var app = $('#app') // 整个视口内容
   menu.attr('data-show', '0') // 默认标记位隐藏 (0 为隐藏，1 为 显示)
   /**
@@ -12,6 +13,10 @@
       'right': '20%'
     }, 400);
     app.animate(({
+      'left': '80%'
+    }))
+    /*解决uc手机头部fixed问题*/
+    headFixed.animate(({
       'left': '80%'
     }))
     appShade.addClass('active')
@@ -25,6 +30,10 @@
       'right': '100%'
     }, 400);
     app.animate(({
+      'left': '0'
+    }))
+    /*解决uc手机头部fixed问题*/
+    headFixed.animate(({
       'left': '0'
     }))
     appShade.removeClass('active')
